bb63ce2fe2cdd88b9efc45dd870ceba99ea1b0a9
KevinBaoB <kevinbaobui@gmail.com>
2022-05-27 09:33:17 -0500

    First

--- js/anagram2.js
+++ js/anagram2.js
@@ -1,3 +1,14 @@
-exports.anagramsFor = function(word, listOfWords) {
-
+exports.anagramsFor = function (word, listOfWords) {
+  let checker = word.split("").sort().join("");
+  let res = [];
+  for (let word of listOfWords) {
+    if (word.split("").sort().join("") === checker) {
+      res.push(word);
+    }
+  }
+  return res;
 };
+
+// console.log(
+//   anagramsFor("saltier", ["cognac", "saltier", "realist", "retails"])
+// );
